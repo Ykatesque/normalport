c77cd240e09f4c6c37c7786461974aa39c0568d4
Yekaterina Novikova <yanoviko@ucdavis.edu>
2013-08-29 09:38:13 -0700

    moving bg

--- js/script.js
+++ js/script.js
@@ -49,8 +49,33 @@ portfoliolink.on('click', scrollPort);
 contactlink.on('click', scrollContact);
 aboutlink.on('click', scrollAbout);
 
+// window.setInterval(function(){
+//    window.setTimeout(function(){
+//        $('.display-sweet-image-behind').css('background-image','url(intoxicating_by_lexiibabii01-d53vl31.jpg)');
+//    },600);
+//     $('.display-sweet-image-behind').css('background-image','url(tumblr_md05w7QDZV1qkovyeo1_500.png)');
+// },300);
 
+var scrollSpeed = 1;
 
+    // set the default position
+    var current = 0;
+
+    // set the direction
+    var direction = 'h';
+
+    function bgscroll(){
+
+        // 1 pixel row at a time
+        current -= 1;
+
+        // move the background with backgrond-position css properties
+        $('.display-sweet-image-behind ').css("backgroundPosition", (direction == 'h') ? current+"px 0" : "0 " + current+"px");
+
+    }
+
+    //Calls the scrolling function repeatedly
+     setInterval(bgscroll, scrollSpeed);
 
 
 
