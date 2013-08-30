d531711f6ac1d077bf42872ccb213f08c458c795
Yekaterina Novikova <yanoviko@ucdavis.edu>
2013-08-30 10:23:29 -0700

    started working on nav

--- js/script.js
+++ js/script.js
@@ -56,26 +56,7 @@ aboutlink.on('click', scrollAbout);
 //     $('.display-sweet-image-behind').css('background-image','url(tumblr_md05w7QDZV1qkovyeo1_500.png)');
 // },300);
 
-var scrollSpeed = 1;
 
-    // set the default position
-    var current = 0;
-
-    // set the direction
-    var direction = 'h';
-
-    function bgscroll(){
-
-        // 1 pixel row at a time
-        current -= 1;
-
-        // move the background with backgrond-position css properties
-        $('.display-sweet-image-behind ').css("backgroundPosition", (direction == 'h') ? current+"px 0" : "0 " + current+"px");
-
-    }
-
-    //Calls the scrolling function repeatedly
-     setInterval(bgscroll, scrollSpeed);
 
 
 
