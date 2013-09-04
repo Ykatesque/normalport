9d726e522e6ecc8480daaffaf532e60cbd1ea774
Yekaterina Novikova <yanoviko@ucdavis.edu>
2013-09-04 11:55:40 -0700

    took out js from html file

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+
+//fade in different texture bgs on Ykat logotype
+var scrollSpeed = 2500;
+var currentImage = 1;
+
+function nextImage(currentImage) {
+      currentImage = ((currentImage + 1) % 4); // we have images 1..6 to cycle through
+      if(currentImage == 0) currentImage = 1;
+      console.log(currentImage);
+
+      $('.display-sweet-image-behind').fadeTo(80, 0.55, function() {
+        $(this).css("background-image", "url(" + currentImage + ".jpg)", "opacity", "0.25").fadeTo(80, 1.0);
+      });
+
+      return currentImage;
+}
+
+setInterval(function(){
+  currentImage = nextImage(currentImage);
+}, 600);
+
+
+
+
 $(document).ready(function() {
 
 //setting some variables
@@ -5,6 +29,7 @@ var menu = $('#menu');
 var aboutlink = $('#about')
 var portfoliolink = $('#portfolio');
 var contactlink = $('#contact');
+
 
 //sticky nav
 var stickyNavTop = $('.nav').offset().top;
@@ -30,6 +55,7 @@ $(window).scroll(function() {
 var scrollPort = function(){
   event.preventDefault();
   $('body, html').animate({scrollTop: 880 + 920}, 'slow');
+  aboutlink.addClass('.aboutbg');
 
 };
 
@@ -42,6 +68,7 @@ var scrollContact = function(){
 var scrollAbout = function(){
   event.preventDefault();
   $('body, html').animate({scrollTop: (850)}, 'slow');
+
   //$('.nav').css('background-image', 'url(../intoxicating_by_lexiibabii01-d53vl31.jpg)');
 
 
@@ -59,6 +86,19 @@ aboutlink.on('click', scrollAbout);
 // },300);
 
 
+
+///Project Menu fadeToggle
+
+ $(".link").on("click", function() {
+   // hide all elements under crate
+   event.preventDefault();
+ $('#wrapper').children().hide();
+
+   // now show the element clicked...
+   child_element_name = $(this).data("target");
+   $(child_element_name).fadeToggle();
+
+});
 
 
 });
